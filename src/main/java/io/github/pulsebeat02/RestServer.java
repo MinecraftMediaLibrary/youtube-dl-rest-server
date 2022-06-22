9c3501c0a58d8b553a08587776ac66d79da1180c
BuildTools <unconfigured@null.spigotmc.org>
2022-06-22 15:51:31 -0400

    Added Stop Command

--- src/main/java/io/github/pulsebeat02/RestServer.java
+++ src/main/java/io/github/pulsebeat02/RestServer.java
@@ -2,26 +2,40 @@ package io.github.pulsebeat02;
 
 import spark.Spark;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
 public final class RestServer {
 
   private final CommandExecution executor;
 
-  public RestServer() {
+  public RestServer() throws IOException {
     this.executor = new CommandExecution();
     this.run();
   }
 
-  public void run() {
+  public void run() throws IOException {
     Spark.get("/info", new RequestHandler(this));
-    while (true) {}
+    this.handleShutdown();
+  }
+
+  public void handleShutdown() throws IOException {
+    final InputStream in = System.in;
+    try (final BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8))) {
+      while (!reader.readLine().equals("stop")) {}
+      Spark.stop();
+    }
   }
 
   public CommandExecution getExecutor() {
-    return executor;
+    return this.executor;
   }
 
-  public static void main(String[] args) {
-    System.out.println("API Link: http://localhost:4567/info?url=URL_HERE");
+  public static void main(final String[] args) throws IOException {
+    System.out.println("Server Link: http://localhost:4567/info?url=URL_HERE");
     new RestServer();
   }
 }
